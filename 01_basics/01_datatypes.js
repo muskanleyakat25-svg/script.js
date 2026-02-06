9337dfaa37f1f54d2af93b7f56841ec09ab69224
muskanleyakat25-svg <muskanleyakat25@gmail.com>
2026-02-06 11:24:19 +0000

    variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all js code as newer version
+// alert(3+3) //we are using nodejs not a browser
+// console.log(3
+//     +3) //code readability should be high
+
+    // let name="muskan"  //string datatype
+    // let age=18   //integer datatype
+    // let isloggedIn=false  //boolean datatype
+    console.log(typeof object);
+//DATA-TYPES
+    //number => range 2 to power 53
+    // bigint (use in bigwebsite )
+    // string
+    // boolean
+    // null
+    // undefined
+    // symbol
+    // object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const AccountId=12345
+let Name="muskan"
+var accountemail="muskanoerveen56@gmail.com"
+let accountpassword="muskan"
+accountcity="hmbkk"
+console.log(AccountId);
+let accountstate
+
+/*prefer not to use var
+because of issue in functional scope*/
+console.table([accountcity, AccountId,accountemail,accountpassword,accountstate])

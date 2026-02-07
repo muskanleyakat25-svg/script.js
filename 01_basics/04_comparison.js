d372d80e5e540b246d6766cc927ee3be0088f477
muskanleyakat25-svg <muskanleyakat25@gmail.com>
2026-02-07 08:41:26 +0000

    comprison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(null>0)
+// console.log(null===0)
+// console.log(null>=0)
+// console.log(null<=0)
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined<=0)
+
+// console.log("2">4)
+// console.log("2"<4)
+// console.log(2>"4")
+// console.log("2"==4)
+// console.log(2>"4")
+// console.log("2"==4)   //we have to avoid all these methods because it create confusion
+
+
+// ===  (strict check)
+// console.log("2"===2)

1850c5ecf438a45a884e976a73943f312f9432ea
muskanleyakat25-svg <muskanleyakat25@gmail.com>
2026-02-07 08:00:00 +0000

    conversion operation

---
+++ 01_basics/03_conversion0peration.js
@@ -0,0 +1,71 @@
+// let score="33abcv"
+// console.log(typeof score);
+// console.log(typeof(score ));
+// let valueInNumber=Number(score);
+// //  console.log(typeof(score))
+// console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+// let score=null
+// console.log(typeof score);
+// console.log(typeof(score ));
+// let valueInNumber=Number(score);
+// //  console.log(typeof(score))
+// console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+ 
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score ));
+// let valueInNumber=Number(score);
+// //  console.log(typeof(score))
+// console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+// "33" => 33
+// "33accv" =>NaN(not a number)
+//true =>1; false=>0
+// let isLoggedIn=1
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);    //output is true because 1 is really denotes boolean true
+
+// let isLoggedIn=0
+//  let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);  
+
+// let isLoggedIn="muskan"
+//  let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);  
+
+// let isLoggedIn=""
+//  let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);  
+// 1 =>true, 0=>false, "empty"=>false, string=>true
+
+
+// let someNum=25
+// let stringNumber=String(someNum)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+/*******************************************OPERATION***********************************************/
+// let Value=5
+// let negativeValue=-Value
+// console.log(negativeValue)
+
+//  console.log(2*2)
+// console.log(2**2)//double multiplication means power
+//  console.log(2**3);
+
+//  console.log(2+"3");
+//  console.log("2"+2);
+// console.log(1+2+"3");
+//  console.log("1"+2+3);
+// console.log(1+"3"+2);
+
+console.log(+true)
+console.log(-true)
+console.log(-false)
+console.log(+false)
+
+
+
